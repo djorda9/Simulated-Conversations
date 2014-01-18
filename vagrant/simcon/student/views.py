31d1cab279ddd75a9590ae8653d74f47e4167712
Jason Nelson <jasonnelson1990@hotmail.com>
2014-01-17 22:33:44 -0800

    Adding Student files
    references Tamples table, which doesn't exist yet.

---
+++ vagrant/simcon/student/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+
+def Submission(request):
+    return render(request, 'Student_Submission.html')
+
+def Login(request):
+    return render(request, 'Student_Login.html')
+
+
+
+
+
+
+
+
+
+
