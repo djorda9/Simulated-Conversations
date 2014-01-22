dd45a6f48513fd4ed5bd004cbf13f262614d54de
Jason Nelson <jasonnelson1990@hotmail.com>
2014-01-21 21:45:28 -0800

    Modified models to be more cohesive
    removed reference to template table (doesn't exist yet) and changed
    reference from Researchers to Researcher. misc. clean up on views.py.

--- vagrant/simcon/student/models.py
+++ vagrant/simcon/student/models.py
@@ -1,11 +1,11 @@
 from django.db import models
-from researcher.models import Researchers
+from researcher.models import Researcher
 from template.models import Templates
 
 class StudentAccess (models.Model):
-    studentAccessID = Field.primary_key = True
-    templateID = models.ForeignKey (Templates)
-    researcherID = models.ForeignKey (Researchers)
+    studentAccessID = AutoField(primary_key=True)
+#    templateID = models.ForeignKey (Templates)
+    researcherID = models.ForeignKey (Researcher)
     validationKey = models.CharField(max_length = 50)
     expirationDate = models.DateField()
     def __unicode__(self):
--- vagrant/simcon/student/views.py
+++ vagrant/simcon/student/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 def Submission(request):
     return render(request, 'Student_Submission.html')
 
-def Login(request):
+def StudentLogin(request):
     return render(request, 'Student_Login.html')
 
 
