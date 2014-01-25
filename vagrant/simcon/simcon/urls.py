e24d036d1987737e677e11380409e364871d17fb
Jason Nelson <jasonnelson1990@hotmail.com>
2014-01-24 22:23:04 -0800

    templates moved, website logic updated
    Moved my html templates to the templates folder: It's specified in the
    settings.py but wasn't in the repository because it was empty. Also
    changed the urls.py to include student pages (PLACEHOLDER), and changed
    views.py to do some error checking.
    Also, including my rough python script for filling the databases. not
    tested yet, need to verify what exactly goes in each database.

--- vagrant/simcon/simcon/urls.py
+++ vagrant/simcon/simcon/urls.py
@@ -7,6 +7,8 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'simcon.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-
+    url(r'^student/(?P<ValKey>\d{10})&(?P<TID>\d{10})&(?P<PIID>\d{10})/$', student.views.StudentVideoInstance),
     url(r'^admin/', include(admin.site.urls)),
-)
+	url(r'^student/(?P<VKey>\d{10})/$', student.views.StudentLogin),
+    url(r'^student/submission/$', student.views.Submission),
+
--- vagrant/simcon/student/views.py
+++ vagrant/simcon/student/views.py
@@ -1,10 +1,38 @@
 from django.shortcuts import render
+#from simcom.templates import Templates
+import datetime
+
+def StudentVideoInstance(request):
+    #Somehow get the template ID(TID), Page Instance ID(PIID), and Validation Key(ValKey).
+    #check tID against template table. Check piID against piID of template, and valKey from StudentAccess table
+    #upload to Responses table which PageInstanceID we're at with the current datetime. If all values already exist but the timedate is different,
+    # then the page was refreshed, display an error, and after a few seconds go to beginning of conversation
+    #check for existence of student response instance input. If so, upload to Responses table the response, the dateTime, and the studentName.
+    return render(request, 'Student_Video_Response.html')
+
+def StudentResponseInstance(request):
+    #Somehow get the template ID(tID), Page Instance ID(piID), and Validation Key(ValKey).
+    #check tID against template table. Check piID against piID of template, and valKey from StudentAccess table
+    #upload to Responses table which PageInstanceID we're at with the current datetime. If all values already exist but the timedate is different,
+    # then the page was refreshed, display an error, and after a few seconds go to beginning of conversation
+    return render(request, 'Student_Text_Response.html')
 
 def Submission(request):
     return render(request, 'Student_Submission.html')
 
 def StudentLogin(request):
-    return render(request, 'Student_Login.html')
+    try:
+        access = StudentAccess.objects.get(validationKey = VKey)
+    except StudentAccess.Invalid:
+        print "Validation Key is invalid"
+
+    expiration = access.expirationDate
+    currentdate = datetime.datetime.now()
+    if(currentdate < expiration)
+        #template = access.templateID
+        return render(request, 'Student_Login.html')
+    else
+        print "Conversation link has expired"
 
 
 
