d530724fa46cd0444035e6f69206922f02e3ad72
hambone20 <hambone20@gmail.com>
2014-01-29 22:30:39 -0800

    Further dev on template wizard
    I think I've made significant progress, but I'm staring at some session
    variable juggling issues.  Trying to map out a total conversation in
    session variables seems to be a bit nontrivial, I have one
    video/response working, but extending it to the chain of videos needs a
    little thought.  Likely it'll require plugging the working video
    instance+responses etc into a list labeled with the video it relates to.
    I haven't touched on the database stuff yet, as that'll be trivial next
    to mapping the thing out in session.  Everything is rough, especially
    variable consistency.  I've located a rich text plugin, but I haven't
    vetted/installed it yet.
    
    You'll notice that the url is /admin/template-wizard.

--- vagrant/simcon/views.py
+++ vagrant/simcon/views.py
@@ -11,20 +11,85 @@ def Submission(request):
 
 def Login(request):
     return render(request, 'Student_Login.html')
-
+    
 @permission_required('simcon.authLevel1')
 def TemplateWizard(request):
     if request.POST:
-        if request.POST.get('addvideobutton') and request.POST['sbutton'] == "Add Video":
+        if request.POST.get('addVideoToPool'): # and request.POST['sbutton'] == "Add Video":  
+            '''
+            User has demanded to add a video to the pool in the left pane
+            '''
             request.session['videos'].append(request.POST['new_video']) #TODO grab code from long youtube url
             request.session.modified = True
-        elif request.POST.get('remove_video'):
-            request.session['videos'].remove(request.POST['remove_video'])
+        elif request.POST.get('removeVideoFromPool'):
+            '''
+            User has demanded to delete a video from the pool in the left pane
+            '''
+            request.session['videos'].remove(request.POST['removeVideoFromPool'])
             request.session.modified = True
         elif request.POST.get('submit'):
-            return HttpResponse("submitted via picture %s" % request.POST.get('submit'))
+            '''
+            User has selected a video from the pool to use for a conversation
+            '''
+            # TODO should we catalog the first video selected?
+            # TODO need to save existing options, video chains somehow
+            selectedVideo = request.POST.get('submit')
+            request.session['selectedVideo'] = selectedVideo # add to selected video TODO may need to confirm?
+            # init options releated to this video, not sure if videos will be singular for sure in a conversation?
+            request.session['responseOptions'] = ['Enter your option'] 
+            request.session.modified = True
+        elif request.POST.get('removeVideoResponse'):
+            '''
+            User has requested that the active video be removed
+            '''
+            #TODO clean up session of all things related to this video
+            del request.session['selectedVideo']
+            del request.session['responseOptions']
+            request.session.modified = True
+        elif request.POST.get('addoption'):
+            '''
+            User has requested to add a new option to the conversation
+            '''
+            #load responseOptions
+            i = 1
+            while request.POST.get('response%d' % i):  # cycle through existing options and load them into responseOptions
+                request.session['responseOptions'][i-1] = request.POST.get('response%d' % i)
+                i += 1
+            request.session.modified = True
+            request.session['responseOptions'].append('Enter your option') #add an option
+            request.session.modified = True
+        elif request.POST.get('removeResponse'):
+            '''
+            User has requested a response option be removed
+            '''
+            #load responseOptions
+            i = 1
+            while request.POST.get('response%d' % i):  # cycle through existing options and load them into responseOptions
+                request.session['responseOptions'][i-1] = request.POST.get('response%d' % i)
+                i += 1
+            request.session.modified = True
+            #TODO if response size < 2, don't do this
+            res = request.POST.get('removeResponse')
+            res = res.split(' ')
+            res = int(res[-1])-1 # should be number of response to remove
+            request.session['responseOptions'].pop(res) # remove this instance
+            request.session.modified = True
+        elif request.POST.get('addVideoToResponse'):
+            '''
+            User has requested that a video be added to this response option, involves redirecting the either video click
+            '''
+            #load responseOptions
+            i = 1
+            while request.POST.get('response%d' % i):  # cycle through existing options and load them into responseOptions
+                request.session['responseOptions'][i-1] = request.POST.get('response%d' % i)
+                i += 1
+            request.session.modified = True
+            res = request.POST.get('addVideoToResponse')
+            res = res.split(' ')
+            res = int(res[-1])-1 # should be number of response to remove
+            return HttpResponse('add %d' % res)
         else:
-            return HttpResponse("uh oh")
+            return HttpResponse("ill formed request")
     else:
         request.session['videos'] = [] # creating an empty list to hold our videos
         request.session['videos'].append('zJ8Vfx4721M')  # sample video
