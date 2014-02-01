7f1cc4bf57a0da418d9d8b3d162153d5678ea996
Jason Nelson <jasonnelson1990@hotmail.com>
2014-01-31 18:35:11 -0800

    Nearly finished with Student Login
    the Student_Login.html needs to be shared to be verified. Also, the
    views.py that loads that html has been updated

--- vagrant/simcon/student/views.py
+++ vagrant/simcon/student/views.py
@@ -1,12 +1,42 @@
+from django.template import loader, Context
 from django.shortcuts import render
-#from simcom.templates import Templates
+from simcom.models import Template
 import datetime
 
 def StudentVideoInstance(request):
-    #Somehow get the template ID(TID), Page Instance ID(PIID), and Validation Key(ValKey).
-    #check tID against template table. Check piID against piID of template, and valKey from StudentAccess table
+    # Get the template ID(TID), Page Instance ID(PIID), and Validation Key(ValKey) as  variables from the url
+    # Check tID against template table. Check piID against piID of template, and valKey from StudentAccess table
+    try:
+        templ = Template.objects.get(templateID = TID)
+    except Template.Invalid:
+        print "Template ID is invalid"
+
+    if(PIID != templ.pageInstanceID)
+        print "Page Instance ID is invalid"
+
+    try:
+        valid = StudentAccess.objects.get(validationKey = VKey)
+    except StudentAccess.Invalid:
+        print "Validation Key is invalid"
+
+    # if student name/email exist, then update the Conversation model
+    if(SName && SEmail) {
+        T = Conversation(template=TID, researcherID = TID.researcherID, studentName = SName, studentEmail = SEmail, dateTime = datetime.datetime.strptime(datetime.datetime.now(), "%Y-%m-%d %H:%M"))
+        T.save()
+    }
+
+
     #upload to Responses table which PageInstanceID we're at with the current datetime. If all values already exist but the timedate is different,
     # then the page was refreshed, display an error, and after a few seconds go to beginning of conversation
+#class Response(models.Model):
+#       pageInstanceID  = models.ForeignKey(PageInstance) 
+#        conversationID  = models.ForeignKey(Conversation)
+#        order           = models.SmallIntegerField()
+#        choice          = models.CharField(max_length=1000)
+#        audioFile       = models.FileField(upload_to='test')
+#    S = Response(pageInstanceID=PIID, conversationID = T, studentEmail = SEmail, dateTime = datetime.datetime.strptime(testDate, "%Y-%m-%d %H:%M"))te
+#    T.save()
+
     #check for existence of student response instance input. If so, upload to Responses table the response, the dateTime, and the studentName.
     return render(request, 'Student_Video_Response.html')
 
@@ -31,7 +61,16 @@ def StudentLogin(request):
     if(currentdate < expiration)
         template = access.templateID
         pageinstance = template.firstInstanceID
-        return render(request, 'Student_Login.html', {'TID':template}, {'PIID':pageinstance}, {'ValKey':VKey})
+
+        t = loader.get_template('Student_Login.html')
+        c = Context({
+        'TID': template,
+        'PIID': pageinstance,
+        'ValKey': VKey,
+        'message': 'I am the Student Login View.'
+        })
+        return t.render(c)
+    }
     else
         print "Conversation link has expired"
 
