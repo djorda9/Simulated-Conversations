0baa066a6c301f069e31eb0dfe8d600e2225580f
Jason Nelson <jasonnelson1990@hotmail.com>
2014-02-09 17:53:28 -0800

    adding template for Boy Factory stuff
    Creating my own database filling script isn't working. various options
    have arisen, the recent attempt uses Boy Factory

--- vagrant/simcon/tests.py
+++ vagrant/simcon/tests.py
@@ -1,4 +1,33 @@
-#researcher tests
+# tests.py
+import random
+import string
+
+import factory
 from django.test import TestCase
 
-# Create your tests here.
+import models
+
+
+def random_string(length=10):
+    return u''.join(random.choice(string.ascii_letters) for x in range(length))
+
+#create one for each model
+class ResearchFactory(factory.DjangoModelFactory):
+    FACTORY_FOR = Researcher
+
+    name = factory.LazyAttribute(lambda t: random_string())
+    description = factory.LazyAttribute(lambda t: random_string())
+
+
+class ResearcherTestCase(TestCase):
+
+    def test_something(self):
+        # Get a completely random thing
+        thing = ResearcherFactory.create()
+        # Test assertions would go here
+
+    def test_something_else(self):
+        # Get a thing with an explicit name
+        thing = ThingFactory.create(name='Foo')
+        # Test assertions would go here
+
