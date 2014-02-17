f4e0b3f5834fcbabdbedee4cb88fcd79da9c0b0e
Alexander Steiert <alexandersteiert@gmail.com>
2014-02-17 15:37:32 -0800

    fixed it up a bit, I fail at syntax, simpler
    The more I learn, the less I'm sure I know.
    Needed base template for login, since it's default redirect is to there.
    Admin site/whatnot has everything else. If future problems, can always
    make templates(or hunt them down) and toss into registration folder.

--- vagrant/simcon/urls.py
+++ vagrant/simcon/urls.py
@@ -1,24 +1,21 @@
 from django.conf.urls import patterns, include, url
 #from django.conf import settings
-
 from django.contrib import admin
+
 admin.autodiscover()
    # Examples:
    # url(r'^$', 'simcon.views.home', name='home'),
    # url(r'^blog/', include('blog.urls')),
 urlpatterns = patterns('simcon.views',
-    url(r'^login/$', 'django.contrib.auth.views.login', name='login'),
-    url(r'^logout/$', 'django.contrib.auth.views.logout', name='logout'),
-    url(r'^password_change/$', 'django.contrib.auth.views.password_change', name='password_change'),
-    url(r'^password_change/done/$', 'django.contrib.auth.views.password_change_done', name='password_change_done'),
     url(r'^admin/template-wizard/$', 'TemplateWizard'),
     url(r'^generatelink/$', 'GenerateLink', name="GenerateLink"),
     url(r'^sharetemplate/$', 'ShareTemplate', name="ShareTemplate"),
     url(r'^links/$', 'Links', name="Links"),
+    url(r'^accounts/', include('django.contrib.auth.urls')),
     )
 
 urlpatterns += patterns('',
-        url(r'^admin/', include(admin.site.urls)))
+    url(r'^admin/', include(admin.site.urls))        )
 
 #if settings.DEBUG:
 #    import debug_toolbar
