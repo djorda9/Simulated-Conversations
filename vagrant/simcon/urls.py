653e0d232c22464e1ba70141317c5ebf58608adb
Alexander Steiert <alexandersteiert@gmail.com>
2014-02-17 15:37:32 -0800

    fixed it up a bit, I fail at syntax, simpler
    The more I learn, the less I'm sure I know.
    Needed base template for login, since it's default redirect is to there.
    Admin site/whatnot has everything else. If future problems, can always
    make templates(or hunt them down) and toss into registration folder.

--- vagrant/simcon/urls.py
+++ vagrant/simcon/urls.py
@@ -1,21 +1,18 @@
 from django.conf.urls import patterns, include, url
 #from django.conf import settings
-
 from django.contrib import admin
+
 admin.autodiscover()
    # Examples:
    # url(r'^$', 'simcon.views.home', name='home'),
    # url(r'^blog/', include('blog.urls')),
 urlpatterns = patterns('simcon.views',
-    url(r'^login/$', 'django.contrib.auth.views.login', name='login'),
-    url(r'^logout/$', 'django.contrib.auth.views.logout', name='logout'),
-    url(r'^password_change/$', 'django.contrib.auth.views.password_change', name='password_change'),
-    url(r'^password_change/done/$', 'django.contrib.auth.views.password_change_done', name='password_change_done'),
     url(r'^admin/template-wizard/$', 'TemplateWizard'),
     url(r'^generatelink/$', 'GenerateLink', name="GenerateLink"),
     url(r'^sharetemplate/$', 'ShareTemplate', name="ShareTemplate"),
     url(r'^shareresponse/$', 'ShareResponse', name="ShareResponse"),
     url(r'^links/$', 'Links', name="Links"),
+<<<<<<< HEAD
 
     url(r'^student/(?P<VKey>\d{10})/$', 'StudentLogin', name = "StudentLogin"),  #, student.views.StudentLogin),
     url(r'^student/video/(?P<ValKey>\d{10})&(?P<TID>\d{10})&(?P<PIID>\d{10})/$', 'StudentVideoInstance', name = "StudentVideoInstance"),  #, student.views.StudentVideoInstance),
@@ -26,6 +23,13 @@ urlpatterns = patterns('simcon.views',
 urlpatterns+=patterns('',
         url(r'^admin/', include(admin.site.urls))
                        )
+=======
+    url(r'^accounts/', include('django.contrib.auth.urls')),
+    )
+
+urlpatterns += patterns('',
+    url(r'^admin/', include(admin.site.urls))        )
+>>>>>>> fixed it up a bit, I fail at syntax, simpler
 
 #if settings.DEBUG:
 #    import debug_toolbar
