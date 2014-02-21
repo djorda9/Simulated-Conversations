8ef64fcf895096a000385689e37732c56815e144
Jason Nelson <jasonnelson1990@hotmail.com>
2014-02-20 16:51:22 -0800

    minor fixes to sql files
    might not matter if we can't resolve foreign key problem

--- vagrant/simcon/sql/conversation.sql
+++ vagrant/simcon/sql/conversation.sql
@@ -1,4 +1,4 @@
-INSERT INTO simcon_Conversation (templateID, researcherID, studentName, studentEmail) VALUES (1,1,"Joseph","Joe@fake.com");
-INSERT INTO simcon_Conversation (templateID, researcherID, studentName, studentEmail) VALUES (2,1,"Rose");
-INSERT INTO simcon_Conversation (templateID, researcherID, studentName, studentEmail) VALUES (3,2,"Ally","Ally@fake.com");
-INSERT INTO simcon_Conversation (templateID, researcherID, studentName, studentEmail) VALUES (4,2,"Henry");
+INSERT INTO simcon_Conversation (templateID, researcherID, studentName, studentEmail, dateTime) VALUES (1,1,"Joseph","Joe@fake.com", "2014-02-20 12:34");
+INSERT INTO simcon_Conversation (templateID, researcherID, studentName, dateTime) VALUES (2,1,"Rose", "2014-02-20 12:34");
+INSERT INTO simcon_Conversation (templateID, researcherID, studentName, studentEmail, dateTime) VALUES (3,2,"Ally","Ally@fake.com", "2014-02-20 12:34");
+INSERT INTO simcon_Conversation (templateID, researcherID, studentName, dateTime) VALUES (4,2,"Henry", "2014-02-20 12:34");
