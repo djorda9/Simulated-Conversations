cff6e016811f1098165bf56f67930a9eb5e63003
Jason Nelson <jasonnelson1990@hotmail.com>
2014-03-09 19:09:10 -0700

    removed button to advance without audio, added expiration to student login page

--- vagrant/simcon/views.py
+++ vagrant/simcon/views.py
@@ -29,8 +29,8 @@ def StudentLogin(request,VKey = 123):
     convo_Expiration = access.expirationDate
     currentdate = datetime.date.today()
     #On other option that is cleaner is to pass the current time and expiration to the template, and have an if statement in the template
-    if(True):
-    #if(currentdate < convo_Expiration):
+    #if(True):
+    if(currentdate < convo_Expiration):
 #fixme
         try:
             #logger.info(access.templateID.templateID)
